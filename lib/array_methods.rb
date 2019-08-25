938375efdd449bab0dfa688996c6bc8846accdd2
Janay Bailey <tjanay87@gmail.com>
2019-08-25 14:25:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ array_incude(element)
 end
 
 def using_sort(array)
-array_sort
+array_sort()
 end
 
 def using_reverse(array)
-array_reverse
+array_reverse()
 end
 
 def using_first(array)
-array_first
+array_first()
 end
 
 def using_last(array)
-array_last
+array_last()
 end
 
 def using_size(array)
-array_size
+array_size()
 end
